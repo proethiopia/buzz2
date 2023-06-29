eb762a7168a2e12696e6498b23886dba926b4a6d
proethiopia <mintesnotbissare@gmail.com>
2023-06-29 00:46:33 -0400

    weather

--- weather.sql
+++ weather.sql
@@ -1,8 +1,14 @@
-create table weather (
+insert into weather (temp_id,record_date, temperature)
+values (1,'2015-01-01', 10 );
+insert into weather (temp_id,record_date, temperature)
+values (2,'2015-01-02', 25 );
+insert into weather (temp_id,record_date, temperature)
+values ( 3,'2015-01-03', 20 );
+insert into weather (temp_id,record_date, temperature)
+values (4,'2015-01-04', 30 );
 
-temp_id int,
-record_date date,
-temperature int
-);
-select temp_id from weather 
-where temperature = ( select max(temperature) from weather)
+
+select  s.temp_id from weather s
+ join weather d on s.record_date   = d.record_date +  interval '1 day'
+where s.temperature > d.temperature
+select * from weather
